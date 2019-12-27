49c38d1a3a71e8e5ef6ae323ba728028b1853145
raskardev <alexcdev95@gmail.com>
2019-12-27 16:58:17 +0100

    feat(): create auth utilities

---
+++ src/utils/auth.js
@@ -0,0 +1,8 @@
+const bcrypt = require('bcrypt');
+
+const encryptPassword = async (password) => {
+  const encryptedPassword = await bcrypt.hash(password, 10);
+  return encryptedPassword;
+};
+
+module.exports = { encryptPassword };
